877d03ce901fe648e621d9f22309a3d79f278a2c
Thomas Walsh <tom.walsh.92@gmail.com>
2015-11-10 08:34:06 -0600

    Removed all the websites we can't release with the tool, and a few other little removals.

--- testing/bbcnews.co.uk/Home - BBC News_files/_footerpromo.min.js
+++
@@ -1,4 +0,0 @@
-/*! Barlesque version 4, orb/api
- *  Copyright (c) 2014 BBC, all rights reserved.
- */
-define("orb/async/_footerpromo",["orb/lib/_$"],function(e){"use strict";var t=3e3,n=function(t,n,r){return e.addCSS(n)&&e.addHTML(t,r)},r={load:function(r,i,s){var o=s.onSuccess||function(){},u=s.onError||function(){},a=s.onAlways||function(){},f,l;f=function(e){e&&e.status==="success"?n(i,e.style,e.html)?o(e):u():u(),a()},l={timeout:t,error:function(){u(),a()},callbackName:"navpromo"},e.script.jsonp(r,f,l)},_ioc:function(t){e=t.$}};return r});
