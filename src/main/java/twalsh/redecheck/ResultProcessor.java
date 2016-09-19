19fe54130d8680a1850e41ea29435d391cc536f0
Thomas Walsh <twalsh1@sheffield.ac.uk>
2016-09-19 16:00:58 +0100

    Syncing
    Former-commit-id: 7aa3d167a767e9b68e289b3a60cca127d1639608

--- src/main/java/twalsh/redecheck/ResultProcessor.java
+++ src/main/java/twalsh/redecheck/ResultProcessor.java
@@ -834,12 +834,40 @@ public class ResultProcessor {
 			String actualFaultCount = getActualFaults(mostRecentRun);
 			if (errorCount != -1) {
 				String classificationString = getClassification(mostRecentRun);
-				System.out.println(webpage + classificationString + " & " + actualFaultCount + " \\\\");
+//				System.out.println(webpage + classificationString + " & " + actualFaultCount + " \\\\");
 			}
 
-			for (int i = 1; i <= 5; i++) {
-				multiTimeData += webpage + "," + i + "," + getMultiExecutionTime(webpage, i) + "\n";
+//			for (int i = 1; i <= 30; i++) {
+//				multiTimeData += webpage + "," + i + "," + getMultiExecutionTime(webpage, i) + "\n";
+//			}
+
+
+//			File fl = new File(dir);
+			File[] runfiles = f.listFiles(new FileFilter() {
+				public boolean accept(File file) {
+					return file.isDirectory();
+				}
+			});
+			System.out.println(f.getAbsolutePath() + "  " + runfiles.length);
+			HashMap<Integer, ArrayList<File>> faultCounts = new HashMap<>();
+			for (File ff : runfiles) {
+				int ec = getErrorCount(ff);
+				if (!faultCounts.containsKey(ec)) {
+					faultCounts.put(ec, new ArrayList<File>());
+				}
+				faultCounts.get(ec).add(ff);
 			}
+			if (faultCounts.entrySet().size() > 1) {
+				for (Integer i : faultCounts.keySet()) {
+					if (faultCounts.get(i).size() == 1) {
+						for (File dodgy: faultCounts.get(i)) {
+							System.out.println(dodgy);
+						}
+					}
+					System.out.println(i + " faults were reported " +faultCounts.get(i).size() + " times");
+				}
+			}
+			System.out.println();
 		}
 //		for (String wp : webpages) {
 //			WebpageMutator mutator = new WebpageMutator(wp+"/index.html", wp, 0);
@@ -852,7 +880,7 @@ public class ResultProcessor {
 //		System.out.println(multiTimeData);
 //		System.out.println(subjectData);
 //		writeToFile(timeData, redecheck+"icst-processing/", "timeData.csv");
-//		writeToFile(multiTimeData, redecheckicst+"icst-processing/", "timing-data.csv");
+//		writeToFile(multiTimeData, redecheckicst+"icst-processing/", "timing-data-new.csv");
 
 //		rp.writeRQ1and2Data(files);
 	}
