9fd535258d1e26b9e4bec97273a1f9b7b970cb95
Thomas Walsh <twalsh1@sheffield.ac.uk>
2016-10-03 12:57:33 +0100

    Syncing hopefully.
    Former-commit-id: ef001bd7bee11a0f7ccf739054168d314c0299bd

--- icst-processing/icst-processing.R
+++ icst-processing/icst-processing.R
@@ -2,13 +2,25 @@ library(ggplot2)
 library(grid)
 library(reshape)
 library(dplyr)
-timeData <- read.csv("timing-data.csv", header=FALSE, sep = ",")
+timeData <- read.csv("timing-data-new.csv", header=FALSE, sep = ",")
 names(timeData) <- c("Webpage", "Iteration", "Model", "Failure", "Report")
-honeyData <- filter(timeData, Webpage=="AirBnb")
+
 modelData <- timeData[,c("Webpage","Iteration","Model")]
-
-
-
-melted <- melt(timeData, id.vars = c("Webpage", "Iteration"))
-plot <- ggplot(melted, aes(x=Webpage, y=value, colour=Webpage)) + geom_boxplot() + facet_grid(. ~ variable)
+plot <- ggplot(modelData, aes(x=Webpage, y=Model, colour=Webpage)) + geom_boxplot()
 print(plot)
+# melted <- melt(timeData, id.vars = c("Webpage"))
+# names(melted) <- c("Webpage", "Stage", "Time")
+# 
+# barplot <- ggplot(data=melted, aes(x=Webpage, y=Time, fill=factor(Stage))) + 
+#   geom_bar(width=.85, stat="identity") +
+#   theme_bw(base_size = 12) +
+#   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+#   theme(legend.position = "top",
+#         legend.text = element_text(size = 12),
+#         legend.box = "horizontal",
+#         legend.key = element_blank(),
+#         legend.key.size = unit(.5, "cm")) +
+#   ylab("Execution Time (seconds)") +
+#   xlab("Web Page") +
+#   guides(fill = guide_legend(nrow = 1))
+# print(barplot)
